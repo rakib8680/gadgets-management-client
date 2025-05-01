e694cfc8b4b09517538923e7b499bda8a8f4a580
rakib8680 <rakib.cst.1st@gmail.com>
2025-05-01 20:16:37 +0600

    Integrate base API with Redux store and add authentication endpoints

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,15 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (loginInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: loginInfo,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
