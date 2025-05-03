b660c5efd8f51efb871bbdb3d5b2306094ee7554
rakib8680 <rakib.cst.1st@gmail.com>
2025-05-03 19:47:33 +0600

    Add Login Functionality Add Sonner for toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/route";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <RouterProvider router={router} />
+    <Toaster />
   </Provider>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,38 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useLoginMutation } from "@/redux/features/auth/authApi";
+import { useAppDispatch } from "@/redux/hooks";
 import { TLoginInput } from "@/types/auth";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { toast } from "sonner";
+
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const { register, handleSubmit,reset } = useForm<TLoginInput>();
+  const { register, handleSubmit, reset } = useForm<TLoginInput>();
+  const [login] = useLoginMutation();
+  // const dispatch = useAppDispatch();
 
-  const onSubmit: SubmitHandler<TLoginInput> = (data) => {
-    console.log(data);
-    reset();
-  }
+  const onSubmit: SubmitHandler<TLoginInput> = async (
+    loginInfo: TLoginInput
+  ) => {
+    const toastId = toast.loading("Loading...");
+
+    try {
+      const res = await login(loginInfo).unwrap();
+      if (res.success) {
+        toast.success("Login successful", { id: toastId, duration: 2000 });
+        reset();
+      }
+      // const { user, token } = res.data;
+      // dispatch(setUser({user,token}))
+      // console.log(user,token);
+    } catch (error) {
+      toast.error("Invalid Email or Password", { id: toastId, duration: 2000 });
+    }
+  };
 
   return (
     <div className="flex items-center justify-center h-screen bg-gradient">
@@ -40,7 +60,7 @@ const Login = () => {
               onClick={() => setShowPassword(!showPassword)}
               className="absolute right-3 bottom-4 cursor-pointer"
             >
-              {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+              {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
             </div>
           </div>
           <Button type="submit">Login</Button>
