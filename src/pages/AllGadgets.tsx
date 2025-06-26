b9137b0b9ed4170c951205b5f1586d573236c030
rakib8680 <rakib.cst.1st@gmail.com>
2025-06-26 22:30:26 +0600

    feat: implement LoadingHamster component for data loading implement getAllGadgets Api

--- src/pages/AllGadgets.tsx
+++ src/pages/AllGadgets.tsx
@@ -1,10 +1,23 @@
+import LoadingHamster from "@/components/ui/loading-hamster/LoadingHamster";
+import { useGetAllGadgetsQuery } from "@/redux/features/productsApi";
+import { TMeta, TProduct } from "@/types/product";
 
 const AllGadgets = () => {
+  const { data, error, isFetching, isLoading } = useGetAllGadgetsQuery({});
+
+  const AllGadgets: TProduct = data?.data || [];
+  const meta: TMeta = data?.meta || {};
+  console.log(isFetching);
+
+  if (isFetching) {
+    return <LoadingHamster />;
+  }
+
   return (
-     <div>
-         <h1>This is AllGadgets component</h1>
-     </div>
-  )
+    <div>
+      <h1>This is AllGadgets component</h1>
+    </div>
+  );
 };
 
 export default AllGadgets;
---
+++ src/types/product.ts
@@ -0,0 +1,60 @@
+type TCategory =
+  | "smartphone"
+  | "tablet"
+  | "laptop"
+  | "smartwatch"
+  | "headphone"
+  | "speaker"
+  | "accessory";
+type TOperatingSystem = "iOS" | "Android" | "Windows" | "macOS" | "Linux";
+type TConnectivity =
+  | "WiFi"
+  | "Bluetooth"
+  | "NFC"
+  | "GPS"
+  | "4G"
+  | "5G"
+  | "USB"
+  | "HDMI"
+  | "Thunderbolt";
+
+type TPowerSource = "Battery" | "Plug-in" | "Battery & Plug-in";
+
+export type TFeature = {
+  // cameraResolution?: string;
+  // storageCapacity?: string;
+  // screenSize?: string;
+  [key: string]: string | number | boolean;
+};
+
+export type TDimensions = {
+  width: number;
+  height: number;
+  depth: number;
+};
+
+export type TProduct = {
+  name: string;
+  price: number;
+  imageURL: string;
+  seller_id: string;
+  quantity: number;
+  releaseDate: Date;
+  brand: string;
+  modelNo: string;
+  category: TCategory;
+  operatingSystem?: TOperatingSystem;
+  connectivity: TConnectivity[];
+  powerSource: TPowerSource;
+  features: TFeature;
+  weight?: number;
+  dimensions?: TDimensions;
+  compatibility?: string[];
+};
+
+export type TMeta = {
+  limit: number;
+  page: number;
+  total: number;
+  totalPage: number;
+};
